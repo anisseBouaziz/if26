7cf99fe94dce07640cdc1868540411caae2bf9d2
Anisse <anisse.bouaziz@gmail.com>
2014-12-09 17:06:51 +0100

    Inscription

---
+++ if26_project/src/fr/utt/if26/activity/InscriptionActivity.java
@@ -0,0 +1,17 @@
+package fr.utt.if26.activity;
+
+import fr.utt.if26.R;
+import fr.utt.if26.R.layout;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class InscriptionActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_inscription);
+	}
+}
